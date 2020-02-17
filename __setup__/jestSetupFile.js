13f960f53705c4753a9a6075a99d42f73e9765bd
Andreas Bergqvist <andreas@itiden.se>
2020-02-17 18:33:17 +0100

    Add codepush mocks

--- __setup__/jestSetupFile.js
+++ __setup__/jestSetupFile.js
@@ -11,3 +11,25 @@ jest.mock('mobx-persist', () => {
   RealComponent.create = config => (key, store) => {};
   return RealComponent;
 });
+jest.mock('react-native-code-push', () => {
+  const cp = (_) => (app) => app;
+  Object.assign(cp, {
+    InstallMode: {},
+    CheckFrequency: {},
+    SyncStatus: {},
+    UpdateState: {},
+    DeploymentStatus: {},
+    DEFAULT_UPDATE_DIALOG: {},
+
+    checkForUpdate: jest.fn(),
+    codePushify: jest.fn(),
+    getConfiguration: jest.fn(),
+    getCurrentPackage: jest.fn(),
+    getUpdateMetadata: jest.fn(),
+    log: jest.fn(),
+    notifyAppReady: jest.fn(),
+    notifyApplicationReady: jest.fn(),
+    sync: jest.fn(),
+  });
+  return cp;
+});
