77af8f06fdfa7185d2d974e4acb8438fc3ee44d4
Andreas Bergqvist <andreas@itiden.se>
2020-02-19 19:23:36 +0100

    Remove Button snapshot test

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,15 +1,11 @@
 // Hello.spec.js
 import React from 'react';
-import {render, shallow} from 'react-native-testing-library';
+import {render} from 'react-native-testing-library';
 import {Button} from './Button';
 
 describe('Button', () => {
   it('renders correct', () => {
     const {queryByText} = render(<Button label="Button" onPress={() => {}} />);
     expect(queryByText('Button')).not.toBeNull();
-  });
-  it('should match to snapshot', () => {
-    const component = shallow(<Button label="Button" onPress={() => {}} />);
-    expect(component).toMatchSnapshot();
   });
 });
