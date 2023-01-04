6c2ef92be86aeaa8c40d36246a6ea2ea9818147d
Andreas Bergqvist <andreas@itiden.se>
2023-01-04 13:23:46 +0100

    Init new 0.71 repo

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -5,6 +5,8 @@
 import 'react-native';
 import React from 'react';
 import App from '../src/App';
+
+// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
--- ios/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  GuessTheRepo
-//
-//  Created by Andreas Bergqvist on 2020-02-12.
-//  Copyright © 2020 Facebook. All rights reserved.
-//
-
-import Foundation
