c547d6eb33c343a99104f74e1ca5348fa8c17e27
Andreas Bergqvist <andreas@itiden.se>
2023-06-21 12:04:45 +0200

    Update RN 0.71.11

--- ios/GuessTheRepo/AppDelegate.mm
+++ ios/GuessTheRepo/AppDelegate.mm
@@ -10,6 +10,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"guesstherepo";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+
   [super application:application didFinishLaunchingWithOptions:launchOptions];
   [RNBootSplash initWithStoryboard:@"BootSplash" rootView:self.window.rootViewController.view];
   return YES;
