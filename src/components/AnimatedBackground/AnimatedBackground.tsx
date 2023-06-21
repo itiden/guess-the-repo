08a0efa09fb398eee502c8fcce57f522a5e7cfab
Andreas Bergqvist <andreas@itiden.se>
2023-06-21 15:50:37 +0200

    Update

--- src/components/AnimatedBackground/AnimatedBackground.tsx
+++ src/components/AnimatedBackground/AnimatedBackground.tsx
@@ -3,8 +3,6 @@ import {
   LinearGradient,
   mix,
   Rect,
-  useSharedValueEffect,
-  useValue,
   vec,
 } from '@shopify/react-native-skia';
 import { styled } from 'nativewind';
@@ -22,22 +20,17 @@ const StyledCanvas = styled(Canvas);
 export const AnimatedBackground = () => {
   const width = Dimensions.get('screen').width;
   const height = Dimensions.get('screen').height;
-  const x = useValue(vec(0, 0));
   const progress = useSharedValue(0);
 
   useEffect(() => {
     progress.value = withRepeat(withTiming(1, { duration: 5000 }), -1, true);
   }, [progress]);
 
-  useSharedValueEffect(() => {
-    x.current = vec(0, mix(progress.value, 0, height));
-  }, progress);
-
   return (
     <StyledCanvas className="absolute w-full h-full">
       <Rect x={0} y={0} width={width} height={height}>
         <LinearGradient
-          start={x}
+          start={vec(0, mix(progress.value, 0, height))}
           end={vec(width, height)}
           colors={[colors.violet[100], colors.violet[400]]}
         />
